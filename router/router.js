ec7e16e8710751b946c175db49c1e2f000e3acdd
CheolSeung Lee <lcs960405@naver.com>
2022-10-17 19:44:26 +0900

    react

---
+++ router/router.js
@@ -0,0 +1,77 @@
+const express = require("express");
+
+const router = express.Router();  // express가 갖고있는 기능 중에 router기능 사용
+
+router.get("/plus", function(request, response){ // /plus라우터 기능정의 및 등록
+    console.log("/plus 라우터호출");
+    console.log(parseInt(request.query.num1) + parseInt(request.query.num2));
+
+    response.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});   // 1번
+    response.write("<html>");
+    response.write("<body>");
+    response.write("응답성공<br>");      // 2번
+    response.write("결과값 : " + (parseInt(request.query.num1) + parseInt(request.query.num2)));
+    response.write("</body>");
+    response.write("</html>");
+    response.end();    // 3번
+});
+
+router.get("/cal", (request, response) => {  // // /cal라우터 기능정의 및 등록
+    // 1. 사용자 입력한 값을 가져오기.
+    let num1 = request.query.num1;
+    let num2 = request.query.num2;
+    let cal = request.query.cal;
+
+    console.log(num1 + cal + num2);
+
+    // 사용자가 입력한 기호에 맞는 연산결과값을 브라우저에 출력하시오.
+    response.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
+    response.write("<html>");
+    response.write("<body>");
+    if(cal == "+") {
+        response.write("결과값 : " + (parseInt(num1) + parseInt(num2)));
+    } else if(cal == "-") {
+        response.write("결과값 : " + (parseInt(num1) - parseInt(num2)));
+    } else if(cal == "*") {
+        response.write("결과값 : " + (parseInt(num1) * parseInt(num2)));
+    } else if(cal == "/") {
+        response.write("결과값 : " + (parseInt(num1) / parseInt(num2)));
+    }
+    response.write("</body>");
+    response.write("</html>");
+    response.end(); 
+    
+});
+
+router.post("/Grade", (request, response) => {
+    response.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
+    response.write("<html>");
+    response.write("<body>");
+    let avg = (parseInt(request.body.java) + parseInt(request.body.web) + parseInt(request.body.iot) + parseInt(request.body.android)) / 4;
+
+    response.write("이름 : " + request.body.name + "<br>");
+    response.write("자바 : " + request.body.java + "<br>");
+    response.write("웹 : " + request.body.web + "<br>");
+    response.write("IoT : " + request.body.iot + "<br>");
+    response.write("안드로이드 : " + request.body.android + "<br>");
+    response.write("avg : " + 
+                (parseInt(request.body.java) + parseInt(request.body.web) + parseInt(request.body.iot) + parseInt(request.body.android)) / 4 + "<br>");
+    if(avg >= 95) {
+        response.write("A+");
+    } else if(avg >= 90 && avg/4 <= 94) {
+        response.write("A");
+    } else if(avg >= 85 && avg/4 <= 89) {
+        response.write("B+");
+    } else if(avg >= 80 && avg/4 <= 84) {
+        response.write("B");
+    } else if(avg >= 75 && avg/4 <= 79) {
+        response.write("C");
+    } else if(avg < 75) {
+        response.write("F");
+    }
+    response.write("</body>");
+    response.write("</html>");
+    response.end(); 
+});
+
+module.exports = router;
